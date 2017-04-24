45e6ae523380b370aa95fcfc5877685275508b9c
Wallermadev <wallermadev@gmail.com>
2017-04-24 21:22:31 +0100

    Transaction Tab hooked up

--- App/Components/transaction-panel/transaction-panel.component.js
+++ App/Components/transaction-panel/transaction-panel.component.js
@@ -3,7 +3,29 @@ angular.
 module('transactionPanel').
 component('transactionPanel', {
     templateUrl: 'Components/transaction-panel/transaction-panel.component.html',
-    controller: function TransactionPanelController() {
+    controller: function TransactionPanelController($scope, $http) {
 
+        $http({
+            method: 'POST',
+            url: 'http://localhost:5279/lbryapi',
+            headers: { 'content-type': 'application/json; charset=UTF-8' },
+            data: {
+                "method": "transaction_list",
+            }
+        }).then(successCallback, errorCallback);
+
+        function successCallback(response) {
+            $.each(response.data.result, function(index, value) {
+                console.log(value);
+                $("#txTable")
+                    .append(
+                        "<tr><td>" + value.date + "</td><td>" + value.value + "</td><td><a target=\"_blank\" href=\"https://explorer.lbry.io/tx/" + value.txid + "\">View</a></td></tr>"
+                    );
+            });
+        }
+
+        function errorCallback(error) {
+            console.log(error);
+        }
     }
 });
--- App/Components/withdraw-panel/withdraw-panel.component.js
+++ App/Components/withdraw-panel/withdraw-panel.component.js
@@ -4,6 +4,31 @@ module('withdrawPanel').
 component('withdrawPanel', {
     templateUrl: 'Components/withdraw-panel/withdraw-panel.component.html',
     controller: function WithdrawPanelController($scope, $http) {
+        $scope.submit = function() {
+            $http({
+                method: 'POST',
+                url: 'http://localhost:5279/lbryapi',
+                headers: { 'content-type': 'application/json; charset=UTF-8' },
+                data: {
+                    "method": "send_amount_to_address",
+                    "params": {
+                        "amount": $scope.qty,
+                        "address": $scope.add
+                    }
+                }
+            }).then(successCallback, errorCallback);
 
+            function successCallback(response) {
+                if (response.data.result === true) {
+                    $('form')[0].reset();
+                    alert("Transaction Complete");
+                }
+            }
+
+            function errorCallback(error) {
+                $('form')[0].reset();
+                alert("A error occurred while sending.");
+            }
+        }
     }
 });
